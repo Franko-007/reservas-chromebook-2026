3573a3e4229b93e86f6f3e56a0d06f4529b24bc3
Franko-007 <luis.franko.sp@gmail.com>
2026-05-04 15:29:47 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // ==================== CONFIGURACIÓN ====================
 // IMPORTANTE: Reemplazar esta URL con la URL de tu Web App después de desplegar el código.gs
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxN2XQAMGfzqDHYkAdfdPrkQ6d6Mni72WRZajuQyyewjDhVAdemvVuUrN0SUY54x_o/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby0uMZGft7qpjxFuNhOIZMO2kKofmPEteF240eXzCLDXNCXhAHX0QMsqw_II2lrEA/exec";
 
 const STOCK_MAXIMO = 115;
 const STOCK_REEMPLAZO = 4;
